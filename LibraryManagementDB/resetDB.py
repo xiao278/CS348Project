58cc4ad709526cb294ca229ad47c4cd776b651a6
xiao278 <90475159+xiao278@users.noreply.github.com>
2024-12-05 22:57:05 -0500

    fixed
    why arent pickles generating anymore

--- LibraryManagementDB/resetDB.py
+++ LibraryManagementDB/resetDB.py
@@ -5,5 +5,6 @@ import os
 cur_dir = os.path.dirname(__file__)
 
 exe_sql_file(os.path.realpath(os.path.join(cur_dir,"./schema.sql")))
+exe_sql_file(os.path.realpath(os.path.join(cur_dir,"./indexes.sql")))
 
 populate_db()
