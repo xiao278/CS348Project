adf3260f0df5ce3c5b99cfda998652fe5f9366c3
xiao278 <90475159+xiao278@users.noreply.github.com>
2024-12-09 16:49:17 -0500

    implemented interactive graph fullscreen thingy

--- library-server/query_stats.ts
+++ library-server/query_stats.ts
@@ -3,38 +3,56 @@ import { sequelize } from "./tables";
 import { QueryTypes } from "sequelize";
 
 async function getGenreCounts(top_genres: number) {
-    const genreCounts = await sequelize.query(`
+    const genre_counts = await sequelize.query(`
         SELECT g.genre, g.book_count
         FROM Genres g
         ORDER BY g.book_count DESC
         LIMIT ${top_genres}`,
         {type: QueryTypes.SELECT}
     );
-    return genreCounts
+    return genre_counts
+}
+
+async function getGenreCountStats() {
+    const total_genres = await sequelize.query(`
+        SELECT COUNT(*)
+        FROM Genres`,
+        {type: QueryTypes.SELECT}
+    );
+    const avg_books = await sequelize.query(`
+        SELECT AVG(g.book_count)
+        FROM Genres g`,
+        {type: QueryTypes.SELECT}
+    );
+    return {
+        count: total_genres,
+        avg: avg_books
+    }
 }
 
 async function getCopiesStatusCounts() {
-    const statusCounts = await sequelize.query(`
+    const status_counts = await sequelize.query(`
         SELECT * 
         FROM Copies_Status_Count`,
         {type: QueryTypes.SELECT}
     );
-    return statusCounts;
+    return status_counts;
 }
 
 async function getGenreSimilarity() {
-    const genreSimilarity = await sequelize.query(`
+    const genre_similarity = await sequelize.query(`
         SELECT gs.genre_id1, g1.genre AS genre1, gs.genre_id2, g2.genre as genre2, gs.shared_books
         FROM Genre_Similarity gs JOIN Genres g1 ON gs.genre_id1 = g1.genre_id
         JOIN Genres g2 ON gs.genre_id2 = g2.genre_id`,
         {type: QueryTypes.SELECT}
     );
-    return genreSimilarity;
+    return genre_similarity;
 }
 
 async function getAllStats() {
     const stats = {
         genre_books: await getGenreCounts(20),
+        genre_books_stats: await getGenreCountStats(),
         copy_status: await getCopiesStatusCounts(),
         genre_similarity: await getGenreSimilarity()
     }
--- library-website/src/pages/reader-content/Statistics.tsx
+++ library-website/src/pages/reader-content/Statistics.tsx
@@ -1,6 +1,8 @@
-import React, { PureComponent, useEffect, useState } from 'react';
+import React, { PureComponent, useEffect, useRef, useState } from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import "./Statistics.css"
+import { ForceGraph2D } from 'react-force-graph';
+import { forceManyBody, forceCenter, forceX, forceY} from 'd3';
+import "./Statistics.css";
 
 interface StatisticsProps {
 
@@ -11,11 +13,22 @@ interface LibStatsData {
         genre: string;
         book_count: number;
     }[];
+    genre_books_stats: {
+        count: number;
+        avg: number
+    }
     copy_status: {
         status: string;
         status_count: number;
     }[];
-    genre_similarity: any[];
+    genre_similarity: {
+        genre_id1: number;
+        genre1: string;
+        genre_id2: number;
+        genre2: string;
+        shared_books: number;
+
+    }[];
 }
 
 function DynamicTable({data}) {
@@ -43,9 +56,70 @@ function DynamicTable({data}) {
     )
 }
 
+interface GraphNode {
+    id: string;
+    name: string;
+    val?: number;
+}
+
+interface GraphLink {
+    source: string;
+    target: string;
+    weight?: number;
+}
+
 function Statistics(props: StatisticsProps) {
     const [ libStats, setLibStats ] = useState<LibStatsData>();
+    const [ nodes, setNodes ] = useState<GraphNode[]>();
+    const [ links, setLinks ] = useState<GraphLink[]>();
+    const [ maxLinkWeight, setMaxLinkWeight ] = useState<number>();
 
+    function parseNodes() {
+        const node_set = {}
+        const temp_nodes:GraphNode[] = []
+        libStats?.genre_similarity.map((link) => {
+            const id1 = link.genre_id1.toString()
+            const name1 = link.genre1
+
+            if (node_set[id1] === undefined) {
+                node_set[id1] = 1
+                temp_nodes.push({
+                    id: id1,
+                    name: name1
+                })
+            }
+
+            const id2 = link.genre_id2.toString()
+            const name2 = link.genre2
+            if (node_set[id2] === undefined) {
+                node_set[id2] = 1
+                temp_nodes.push({
+                    id: id2,
+                    name: name2
+                })
+            }
+        })
+        setNodes(temp_nodes)
+    }
+
+    function parseLinks() {
+        const temp_links:GraphLink[] = []
+        let temp_max_weight = 0;
+        libStats?.genre_similarity.map((pair) => {
+            const threshold = 100
+            if (pair.shared_books > threshold) {
+                temp_links.push({
+                    source: pair.genre_id1.toString(),
+                    target: pair.genre_id2.toString(),
+                    weight: pair.shared_books
+                })
+                temp_max_weight = (pair.shared_books > temp_max_weight ? pair.shared_books : temp_max_weight)
+            }
+        })
+        setMaxLinkWeight(temp_max_weight)
+        setLinks(temp_links)
+    }
+ 
     async function fetchStats() {
         let response = await fetch("http://localhost:8080/fetchStats", {
             method: 'GET'
@@ -56,12 +130,24 @@ function Statistics(props: StatisticsProps) {
             }
         });
         setLibStats(response)
+        
     }
 
     useEffect(() => {
-        fetchStats()
-        console.log("fetched stats")
+        if (libStats === undefined) {
+            fetchStats();
+            console.log("fetched stats");
+        }
     },[])
+
+    useEffect(() => {
+        if (nodes === undefined && libStats !== undefined){
+            parseNodes();
+            console.log("parsed nodes");
+            parseLinks();
+            console.log("parsed links")
+        }
+    },[libStats])
 
     function sumCopies() {
         let counter = 0;
@@ -72,7 +158,6 @@ function Statistics(props: StatisticsProps) {
     }
 
     function GraphGenreBooks() {
-        console.log(libStats?.genre_books)
         return (
             <div style={{width: "100%", height: "700px"}}>
                 <ResponsiveContainer width="100%" height="100%">
@@ -89,6 +174,90 @@ function Statistics(props: StatisticsProps) {
         )
     }
 
+    function GraphSimilarity(props: {interact: boolean}) {
+        const fgRef = useRef<any>();
+        const [ fullscreen, setFullScreen ] = useState<boolean>(false);
+        const windowSize = 500
+        const fullSize = 800
+
+        function setZoom(zoom:number, speed:number = 0) {
+            const fg = fgRef.current
+            if (fg === undefined) return;
+            fg.centerAt(0, 0, speed)
+            fg.zoom(zoom, speed)
+        }
+
+        useEffect(() => {
+            const fg = fgRef.current
+            if (fg === undefined) {
+                console.log("fail to initialize forcegraph")
+                return
+            }
+            fg.d3Force('link')?.distance((link:GraphLink) => {
+                const baseWeight = (link.weight === undefined || maxLinkWeight === undefined) ? 0 : link.weight / maxLinkWeight
+                if (baseWeight < 0.5) return 400;
+                return (baseWeight ** 2 * -200) + 200;
+            });
+            fg.d3Force("charge", forceManyBody().strength(-200));
+            fg.d3Force("x", forceX())
+            fg.d3Force("y", forceY())
+            setZoom(fullscreen ? 1 : 0.5)
+            console.log("forcegraph initialized")
+        },[])
+        
+        return (
+            <div style={{height: windowSize, width: windowSize}}>
+                <div className={fullscreen ? 'GraphDisplayFull' : 'GraphDisplay'} onClick={() => {
+                    if (!fullscreen) {
+                        setFullScreen(true)
+                        setZoom(0.9, 1000)
+                    }
+                }}>
+                    {nodes === undefined || links === undefined ? "N/A" : 
+                        <ForceGraph2D 
+                            ref={fgRef}
+                            width={fullscreen ? fullSize: windowSize}
+                            height={fullscreen ? fullSize: windowSize}
+                            
+                            graphData={{nodes: nodes, links: links}} 
+                            enableZoomInteraction={props.interact || fullscreen} 
+                            enablePanInteraction={props.interact || fullscreen} 
+                            enableNodeDrag={props.interact || fullscreen}
+                            // nodeAutoColorBy={"name"}
+                            linkColor={(link) => {
+                                const alpha = ((link.weight === undefined || maxLinkWeight === undefined) ? 0 : link.weight / maxLinkWeight) ** 1.5
+                                return `rgba(249, 255, 166,${alpha})`
+                            }}  
+                            nodeCanvasObject={(node, ctx, globalScale) => {
+                                if (node.x === undefined || node.y === undefined) return;
+                                const label = node.name;
+                                const fontSize = 12/globalScale;
+                                ctx.font = `${fontSize}px Sans-Serif`;
+                                const textWidth = ctx.measureText(label).width;
+                                const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+                    
+                                ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+                                // @ts-ignore
+                                ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                    
+                                ctx.textAlign = 'center';
+                                ctx.textBaseline = 'middle';
+                                ctx.fillStyle = 'rgba(255,255,255,1)';
+                                ctx.fillText(label, node.x, node.y);
+                    
+                                node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+                            }}
+                        />
+                    }
+                    {fullscreen ? 
+                        <button className='Common-Button' style={{position: "fixed", left: "30px", top: "30px", fontSize: "20px"}} onClick={() => {setFullScreen(false); setZoom(0.5, 1000)}}>Close</button>
+                        : <></>
+                    }
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="StatPageContainer">
             <div>
@@ -96,8 +265,14 @@ function Statistics(props: StatisticsProps) {
                 {libStats === undefined ? "N/A" : <DynamicTable data={libStats.copy_status} />}
             </div>
             <div>
-                <h1>Genres with the most books</h1>
+                <h1>Top Genres</h1>
                 {libStats === undefined ? "N/A" : <GraphGenreBooks />}
+            </div>
+            <div>
+                <h1>Genre Similarity (top 100)</h1>
+                <div>
+                    <GraphSimilarity interact={false}/>
+                </div>
             </div>
         </div>
     )
